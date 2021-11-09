16533dfc2b0cffc8add4f8b39234ae0bdf13bfd5
Endika <dash6657@gmail.com>
2021-11-09 13:13:44 +0100

    read db data

--- app/src/main/java/com/example/didaktikapp/activities/Activity1_Principal.kt
+++ app/src/main/java/com/example/didaktikapp/activities/Activity1_Principal.kt
@@ -1,19 +1,34 @@
 package com.example.didaktikapp.activities
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
 import com.example.didaktikapp.R
 
 import com.example.didaktikapp.databinding.Activity1PrincipalBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
+
+import com.google.firebase.firestore.QuerySnapshot
+
+import androidx.annotation.NonNull
+
+import com.google.android.gms.tasks.OnCompleteListener
+
+
+
 
 class Activity1_Principal : AppCompatActivity() {
 
     private lateinit var binding: Activity1PrincipalBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity1_principal)
@@ -22,62 +37,81 @@ class Activity1_Principal : AppCompatActivity() {
         binding = Activity1PrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getData(db)
         menu()
         ocultarbtn()
-        binding.btn1Hasi.setOnClickListener(){
-            binding.btn1Hasi.isVisible=false
-            binding.btn1Cargar.isVisible=true
-            binding.btn1Nuevo.isVisible=true
-            
-            binding.btn1Cargar.setOnClickListener(){
+
+        binding.btn1Hasi.setOnClickListener() {
+            binding.btn1Hasi.isVisible = false
+            binding.btn1Cargar.isVisible = true
+            binding.btn1Nuevo.isVisible = true
+
+            binding.btn1Cargar.setOnClickListener() {
                 ocultartodo()
             }
 
         }
 
     }
-    fun ocultarbtn(){
-        binding.btn1Cargar.isVisible=false
-        binding.btn1Nuevo.isVisible=false
-        binding.btnMapa.isVisible=false
-        binding.btnLogin.isVisible=false
-        binding.btnLoad.isVisible=false
-        binding.btnBienvenida.isVisible=false
+
+    fun ocultarbtn() {
+        binding.btn1Cargar.isVisible = false
+        binding.btn1Nuevo.isVisible = false
+        binding.btnMapa.isVisible = false
+        binding.btnLogin.isVisible = false
+        binding.btnLoad.isVisible = false
+        binding.btnBienvenida.isVisible = false
     }
 
-    fun menu(){
-        binding.btnBienvenida.setOnClickListener(){
+    fun menu() {
+        binding.btnBienvenida.setOnClickListener() {
             var i = Intent(this, Activity4_bienvenida::class.java)
             startActivity(i)
         }
-        binding.btnLoad.setOnClickListener(){
+        binding.btnLoad.setOnClickListener() {
             var i = Intent(this, Activity3_Load::class.java)
             startActivity(i)
         }
-        binding.btnLogin.setOnClickListener(){
+        binding.btnLogin.setOnClickListener() {
             var i = Intent(this, Activity2_Login::class.java)
             startActivity(i)
         }
-        binding.btnMapa.setOnClickListener(){
+        binding.btnMapa.setOnClickListener() {
             var i = Intent(this, Activity5_Mapa::class.java)
             startActivity(i)
         }
 
     }
 
-    fun ocultartodo(){
+    fun ocultartodo() {
         //ocultamos lo que no nos interesa
-        binding.btn1Nuevo.isVisible=false
-        binding.btn1Cargar.isVisible=false
-        binding.btn1Hasi.isVisible=false
-        binding.img1Upelio.isVisible=false
-        binding.txtv1Gruponombre.isVisible=false
-        binding.txtv1Titulo.isVisible=false
+        binding.btn1Nuevo.isVisible = false
+        binding.btn1Cargar.isVisible = false
+        binding.btn1Hasi.isVisible = false
+        binding.img1Upelio.isVisible = false
+        binding.txtv1Gruponombre.isVisible = false
+        binding.txtv1Titulo.isVisible = false
 
         //sacamos lo que nos interesa
-        binding.btnMapa.isVisible=true
-        binding.btnLogin.isVisible=true
-        binding.btnLoad.isVisible=true
-        binding.btnBienvenida.isVisible=true
+        binding.btnMapa.isVisible = true
+        binding.btnLogin.isVisible = true
+        binding.btnLoad.isVisible = true
+        binding.btnBienvenida.isVisible = true
     }
+
+    fun getData(db: FirebaseFirestore) {
+
+        db.collection("Usuarios")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        Log.d(TAG, document.id + " => " + document.data)
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.exception)
+                }
+            }
+    }
+
 }

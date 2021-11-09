3cc003cf0598e3f7c698e1485e1b786998b04d44
Julen <julenarmeex@gmail.com>
2021-11-09 10:26:22 +0100

    Splash screen done

--- app/src/main/java/com/example/didaktikapp/activities/Activity1_Principal.kt
+++ app/src/main/java/com/example/didaktikapp/activities/Activity1_Principal.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.didaktikapp.R
 
+import com.example.didaktikapp.databinding.Activity1PrincipalBinding
+
 class Activity1_Principal : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1_principal)
+
+
+
     }
 }

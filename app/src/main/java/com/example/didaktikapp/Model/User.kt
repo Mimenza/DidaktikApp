d18efdff5f6a485f8f01e2e56c5f5983c5c7b2ef
Julen <julenarmeex@gmail.com>
2021-11-10 13:11:20 +0100

    usuarios cargados en load partida

---
+++ app/src/main/java/com/example/didaktikapp/Model/User.kt
@@ -0,0 +1,12 @@
+package com.example.reto01.Model
+
+
+data class User(
+    var id: Int? = null,
+    var nombre: String? = null,
+    var admin: Int? = null,
+    var puntuacion: Int? = null,
+    var ultima_puntuacion: Int? = null,
+
+)
+

347a95bd22640613e65ec584b44b9d556f8b2b47
Galder <galdergomez50@gmail.com>
2021-11-11 12:49:15 +0100

    navigation a medias

--- app/src/main/java/com/example/didaktikapp/activities/sites/Activity6_1_Sagardoetxea.kt
+++ app/src/main/java/com/example/didaktikapp/activities/sites/Activity6_1_Sagardoetxea.kt
@@ -2,11 +2,14 @@ package com.example.didaktikapp.activities.sites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.didaktikapp.R
 
 class Activity6_1_Sagardoetxea : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide()
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
         setContentView(R.layout.activity6_1_sagardoetxea)
     }
 }
--- app/src/main/java/com/example/didaktikapp/fragments/juegos/Fragment1_3_juego.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/juegos/Fragment1_3_juego.kt
@@ -35,7 +35,7 @@ class Fragment1_3_juego : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment1_juego, container, false)
+        return inflater.inflate(R.layout.fragment1_1_juego, container, false)
     }
 
     companion object {

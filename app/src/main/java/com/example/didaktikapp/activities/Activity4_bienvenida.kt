a2a29f6038433eae6bcc80e725e9253c4c9e2ca2
Julen <julenarmeex@gmail.com>
2021-11-12 12:40:38 +0100

    animacion manzana

--- app/src/main/java/com/example/didaktikapp/activities/Activity4_bienvenida.kt
+++ app/src/main/java/com/example/didaktikapp/activities/Activity4_bienvenida.kt
@@ -1,23 +1,35 @@
 package com.example.didaktikapp.activities
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.media.MediaPlayer
+import android.widget.ImageView
 import com.example.didaktikapp.R
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
 class Activity4_bienvenida : AppCompatActivity() {
+    private lateinit var appleAnimation:AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
         setContentView(R.layout.activity4_bienvenida)
         getSupportActionBar()?.hide()
+
+        //Animacion manzana
+        val appleImage = findViewById<ImageView>(R.id.imgv4_manzanatutorial).apply {
+            setBackgroundResource(R.drawable.animacion_manzana)
+            appleAnimation = background as AnimationDrawable
+        }
+
+        appleImage.setOnClickListener({ appleAnimation.start() })
+        //Animacion manzana fin
 
 
         Handler().postDelayed({

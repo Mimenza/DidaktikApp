92ef2d3482e8c9c3d67d6a27626f18b03e0cc2d4
Endika <dash6657@gmail.com>
2021-11-16 09:40:11 +0100

    boton principal menu + navegacion entre fragments con botones

--- app/src/main/java/com/example/didaktikapp/fragments/juegos/Fragment1_1_juego.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/juegos/Fragment1_1_juego.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.Navigation
 import com.example.didaktikapp.R
 
@@ -38,10 +39,12 @@ class Fragment1_1_juego : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment1_1_juego, container, false)
+        val button: Button = view.findViewById(R.id.btnf1_1siguiente)
 
-        Handler().postDelayed({
+        button.setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_fragment1_1_juego_to_fragment2_1_minijuego)
-        }, 1000)
+        }
+
 
         return view
     }
--- app/src/main/java/com/example/didaktikapp/fragments/minijuegos/Fragment2_1_minijuego.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/minijuegos/Fragment2_1_minijuego.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.Navigation
 import com.example.didaktikapp.R
 
@@ -38,10 +39,12 @@ class Fragment2_1_minijuego : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment2_1_minijuego, container, false)
+        val button: Button = view.findViewById(R.id.btnf2_1siguiente)
 
-        Handler().postDelayed({
+        button.setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_fragment2_1_minijuego_to_fragment4_menu)
-        }, 1000)
+        }
+
         return view
     }
 

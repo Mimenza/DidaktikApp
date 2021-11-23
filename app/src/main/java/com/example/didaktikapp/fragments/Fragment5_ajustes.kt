4d7711bbfc2d391c39135827d5998996a089e890
luisinooooo <73461600+luisinooooo@users.noreply.github.com>
2021-11-23 08:48:13 +0100

    Finished Admin Mode
    Finished Admin Mode

--- app/src/main/java/com/example/didaktikapp/fragments/Fragment5_ajustes.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/Fragment5_ajustes.kt
@@ -6,6 +6,7 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.text.InputType
 import android.text.Layout
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,7 @@ import android.view.Window
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.didaktikapp.R
+import com.example.didaktikapp.activities.DbHandler
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -51,7 +53,13 @@ class Fragment5_ajustes : Fragment() {
 
 
         buttonAcercaDe.setOnClickListener{ showAcercaDeInfo()}
-        buttonAdmin.setOnClickListener{ showdialog()}
+        buttonAdmin.setOnClickListener{
+            if (!DbHandler.getAdmin()) {
+                showModoAdminDialog()
+            } else {
+                Toast.makeText(requireContext(), "ERROR: Ya esta activado el modo administrador", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return view
     }
@@ -67,20 +75,22 @@ class Fragment5_ajustes : Fragment() {
         )
     }
 
-    fun showdialog(){
+    fun showModoAdminDialog(){
         val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(requireContext())
-        builder.setTitle("Admin Password")
+        builder.setTitle("Modo Administrador")
         val input = EditText(requireContext())
-        input.setHint("Enter the admin password")
-        input.inputType = InputType.TYPE_CLASS_TEXT
+        input.setHint("Ingrese la contraseña de administrador")
+        input.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+        input.setTransformationMethod(PasswordTransformationMethod.getInstance());
         builder.setView(input)
 
-        builder.setPositiveButton("Iniciar", DialogInterface.OnClickListener { dialog, which ->
+        builder.setPositiveButton("Logear", DialogInterface.OnClickListener { dialog, which ->
             //var m_Text = input.text.toString()
             if (pass.equals(input.text.toString())) {
-                Toast.makeText(requireContext(), "MODO ADMIN ACTIVADO CORRECTAMENTE", Toast.LENGTH_SHORT).show()
+                DbHandler.setAdmin()
+                Toast.makeText(requireContext(), "Modo Administrador activado correctamente", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(requireContext(), "ERROR MODO ADMIN", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Error al activar el modo administrador", Toast.LENGTH_SHORT).show()
             }
         })
         builder.setNegativeButton("Cancelar", DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })

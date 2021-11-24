ef2f1550b042553a75d84f5f258c68c537d01ee6
Endika <dash6657@gmail.com>
2021-11-24 10:34:34 +0100

    splash zoom test

--- app/src/main/java/com/example/didaktikapp/activities/Activity0_Splash.kt
+++ app/src/main/java/com/example/didaktikapp/activities/Activity0_Splash.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.view.animation.AnimationUtils
 import com.example.didaktikapp.R
 import com.example.didaktikapp.databinding.Activity0SplashBinding
 
@@ -17,15 +18,21 @@ class Activity0_Splash : AppCompatActivity() {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
         setContentView(R.layout.activity0_splash)
 
-
         binding = Activity0SplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        /*Handler().postDelayed({
+            val aniFade = AnimationUtils.loadAnimation(this, R.anim.zoom)
+            binding.txtv0Upelio.startAnimation(aniFade)
+
+        }, 2000)*/
+
         Handler().postDelayed({
+
             val intent = Intent(this, Activity1_Principal::class.java)
             startActivity(intent)
             this.overridePendingTransition(0, 0)
             finish()
-        }, 2000)
+        }, 2200)
     }
 }

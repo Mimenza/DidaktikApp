da0b006986bf3a6a3a142e148caca690d5ddfb68
luisinooooo <73461600+luisinooooo@users.noreply.github.com>
2021-11-29 00:00:56 +0100

    Mecanica Iniciada PUZZLE + RECOGER MANZANAS
    Mecanica Iniciada PUZZLE + RECOGER MANZANAS

--- app/src/main/java/com/example/didaktikapp/fragments/juegos/Fragment1_3_2_juego.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/juegos/Fragment1_3_2_juego.kt
@@ -1,13 +1,18 @@
 package com.example.didaktikapp.fragments.juegos
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.Navigation
+import com.example.didaktikapp.Model.CustomLine
 import com.example.didaktikapp.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -25,6 +30,10 @@ class Fragment1_3_2_juego : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var globalView: View
+
+    private var viewActiva = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -39,8 +48,22 @@ class Fragment1_3_2_juego : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment1_3_2_juego, container, false)
+        globalView = view
         val button: Button = view.findViewById(R.id.btnf1_3_2_siguiente)
         val ajustes: ImageButton = view.findViewById(R.id.btnf1_3_2_ajustes)
+
+
+        val constraintLayoutFound = globalView.findViewById<ConstraintLayout>(R.id.mainlayout)
+        var newView: ImageView
+        newView = ImageView(requireContext())
+        newView.layoutParams.height = 200
+        newView.layoutParams.width = 200
+        newView.x = 300F
+        newView.y = 500F
+        newView.setBackgroundColor(Color.BLUE)
+        //newView.setImageResource(R.drawable.sagarragorria)
+
+        constraintLayoutFound.addView(newView)
 
         button.setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_fragment1_3_2_juego_to_fragment2_3_2_minijuego)
@@ -49,6 +72,17 @@ class Fragment1_3_2_juego : Fragment() {
             Navigation.findNavController(view).navigate(R.id.action_fragment1_3_2_juego_to_fragment4_menu)
         }
         return view
+    }
+
+    fun startTimeCounter(view: View, timeInSeconds: Int) {
+        object: CountDownTimer(50000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                //Each second create an element
+            }
+            override fun onFinish() {
+                //Timer Finished + Might this should show win screen
+            }
+        }.start()
     }
 
     companion object {

1ceb933f1ea9cbb416eb10f22649210df37bbad6
Julen <julenarmeex@gmail.com>
2022-01-12 12:41:29 +0100

    minijuego4 diseño

--- app/src/main/java/com/example/didaktikapp/fragments/minijuegos/Fragment2_4_minijuego.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/minijuegos/Fragment2_4_minijuego.kt
@@ -1,13 +1,18 @@
 package com.example.didaktikapp.fragments.minijuegos
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.navigation.Navigation
+import com.example.didaktikapp.Model.DragnDropImage
 import com.example.didaktikapp.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,6 +29,8 @@ class Fragment2_4_minijuego : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    var manzanaList: MutableList<DragnDropImage>? = mutableListOf()
+    private lateinit var txtAciertos:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +48,14 @@ class Fragment2_4_minijuego : Fragment() {
         val view = inflater.inflate(R.layout.fragment2_4_minijuego, container, false)
         val button: Button = view.findViewById(R.id.btnf2_4siguiente)
         val ajustes: ImageButton = view.findViewById(R.id.btnf2_4ajustes)
+        txtAciertos = view.findViewById((R.id.manzanasAciertos4))
+
+        //manzanas
+        val manzana1: ImageView= view.findViewById(R.id.manzana1_minijuego4)
+        val manzana2: ImageView= view.findViewById(R.id.manzana2_minijuego4)
+        val manzana3: ImageView= view.findViewById(R.id.manzana3_minijuego4)
+        val manzana4: ImageView= view.findViewById(R.id.manzana4_minijuego4)
+        val manzana5: ImageView= view.findViewById(R.id.manzana5_minijuego4)
 
         button.setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_fragment2_4_minijuego_to_fragment4_menu)
@@ -49,8 +64,33 @@ class Fragment2_4_minijuego : Fragment() {
         ajustes.setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_fragment2_4_minijuego_to_fragment4_menu)
         }
+
+        button.visibility=View.GONE
+
+
+        prepairApples()
+
+
+
         return view
     }
+
+   fun prepairApples(){
+
+
+
+   }
+
+
+    private fun findItemByOrigen(view: View): DragnDropImage? {
+        for (item in manzanaList!!) {
+            if (item.origen == view) {
+                return item
+            }
+        }
+        return null
+    }
+
 
     companion object {
         /**

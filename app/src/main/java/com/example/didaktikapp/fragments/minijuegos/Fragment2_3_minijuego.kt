996b9aa6311ab41e1d5d91cacc3748fcdc59484f
Endika <dash6657@gmail.com>
2022-01-12 09:32:20 +0100

    minijuego 3 agua testeo

--- app/src/main/java/com/example/didaktikapp/fragments/minijuegos/Fragment2_3_minijuego.kt
+++ app/src/main/java/com/example/didaktikapp/fragments/minijuegos/Fragment2_3_minijuego.kt
@@ -1,8 +1,6 @@
 package com.example.didaktikapp.fragments.minijuegos
 
 import android.annotation.SuppressLint
-import android.graphics.ColorMatrix
-import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -10,12 +8,15 @@ import android.widget.*
 import androidx.navigation.Navigation
 import com.example.didaktikapp.Model.DragnDropImage
 import com.example.didaktikapp.R
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-lateinit var agua:TextView
+lateinit var agua:ImageView
+lateinit var cesta:ImageView
+
 var manzanaList: MutableList<DragnDropImage>? = mutableListOf()
 
 /**
@@ -31,16 +32,17 @@ class Fragment2_3_minijuego : Fragment() {
     private lateinit var globalView: View
     var minijuegoFinalizado: Boolean = false
     lateinit var  button:Button
-
+    var acertados:Int = 0
     val listaImagenes = listOf(
-        listOf(R.id.imgV2_3CleanApple1,R.id.textView5),
-        listOf(R.id.imgV2_3CleanApple2,R.id.textView5),
-        listOf(R.id.imgV2_3CleanApple3,R.id.textView5),
-        listOf(R.id.imgV2_3CleanApple4,R.id.textView5),
-        listOf(R.id.imgV2_3CleanApple5,R.id.textView5),
+        listOf(R.id.imgV2_3CleanApple1,R.id.imgv2_3agua,R.id.imgv2_3cesta),
+        listOf(R.id.imgV2_3CleanApple2,R.id.imgv2_3agua,R.id.imgv2_3cesta),
+        listOf(R.id.imgV2_3CleanApple3,R.id.imgv2_3agua,R.id.imgv2_3cesta),
+        listOf(R.id.imgV2_3CleanApple4,R.id.imgv2_3agua,R.id.imgv2_3cesta),
+        listOf(R.id.imgV2_3CleanApple5,R.id.imgv2_3agua,R.id.imgv2_3cesta),
 
         //listOf(R.id.puzzle_pieza_otest1,R.id.puzzle_pieza_otest2),
     )
+    private lateinit var aciertosTxt:TextView
 
     var manzanaList: MutableList<DragnDropImage>? = mutableListOf()
 
@@ -63,7 +65,8 @@ class Fragment2_3_minijuego : Fragment() {
         button = view.findViewById(R.id.btnf2_3_1siguiente)
         val ajustes: ImageButton = view.findViewById(R.id.btnf2_3_1ajustes)
 
-        agua  = view.findViewById((R.id.textView4))
+        agua  = view.findViewById((R.id.imgv2_3agua))
+        cesta = view.findViewById((R.id.imgv2_3cesta))
 
         button.setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_fragment2_3_minijuego_to_fragment4_menu)
@@ -113,30 +116,43 @@ class Fragment2_3_minijuego : Fragment() {
                     }
                     MotionEvent.ACTION_UP -> {
 
-                       /* viewElement.x = motionEvent.rawX - viewElement.width/2
+                        viewElement.x = motionEvent.rawX - viewElement.width/2
                         viewElement.y = motionEvent.rawY - viewElement.height/2
                         //var objetivoEncontrado: View = itemInList!!.objetivo
-                        // Basurero Vars
-                        val basureroLocation = IntArray(2)
-                        agua.getLocationOnScreen(basureroLocation);
-                        var basureroPosX = basureroLocation[0]
-                        var basureroPosY = basureroLocation[1]
-                        var basureroSizeX = agua.width
-                        var basureroSizeY = agua.height
-                        // Cesta Vars
+
+                        //=====================================================
+
                         val aguaLocation = IntArray(2)
                         agua.getLocationOnScreen(aguaLocation);
+
+                        //Posicion del agua
                         var aguaPosX = aguaLocation[0]
                         var aguaPosY = aguaLocation[1]
                         var aguaSizeX = agua.width
                         var aguaSizeY = agua.height
+
+                        //=====================================================
+
+                        // Cesta Vars
+                        val cestaLocation = IntArray(2)
+                        cesta.getLocationOnScreen(cestaLocation);
+
+                        //Posicion de la cesta
+                        var cestaPosX = cestaLocation[0]
+                        var cestaPosY = cestaLocation[1]
+                        var cestaSizeX = agua.width
+                        var cestaSizeY = agua.height
+
+                        //=====================================================
                         if ( (viewElement.x + viewElement.width/2) >= aguaPosX && (viewElement.y + viewElement.height/2) >= aguaPosY && (viewElement.x + viewElement.width/2) <= aguaPosX+aguaSizeX && (viewElement.y + viewElement.height/2) <= aguaPosY+aguaSizeY) {
                             comprobarInsercionManzana(itemInList, agua)
                             itemInList.acertado = true
 
-                            viewElement.visibility = View.GONE
+
+                             viewElement.visibility = View.GONE
                             viewElement.setOnTouchListener(null)
-                        }*/
+                        }
+
                     }
                 }
             }
@@ -144,7 +160,7 @@ class Fragment2_3_minijuego : Fragment() {
         true
     }
 
-    private fun comprobarInsercionManzana(item: DragnDropImage, objetivoInsertado: TextView) {
+    private fun comprobarInsercionManzana(item: DragnDropImage, objetivoInsertado: ImageView) {
         //item.acertado = true
         if (item.objetivo == objetivoInsertado) {
             aciertosActuales++
